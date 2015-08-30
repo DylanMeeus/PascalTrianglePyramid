6d244e8429e56268c3a9cb5126f36546f987b10e
Dylan Meeus <meeusdylan@hotmail.com>
2015-08-30 06:22:44 +0200

    Pascal's Pyramid
    Implemented Pascal's pyramid as a simplex to render.

--- js/pascal.js
+++ js/pascal.js
@@ -7,17 +7,64 @@
 
 function generatePascalTriangleRow(row)
 {
-    var numbers = [1];
 
-    var n = row;
-    var k = 1;
+var numbers = [1];
 
-    for(var i = 1; i < row; i++)
-    {
-        numbers[i] = numbers[i-1] * ((n+1-k)/k);
-        k++;
+    if(row == 0){
+        return numbers;
     }
-    numbers[numbers.length] = 1;
-    console.log(numbers);
-    return [numbers];
+
+var n = row;
+var k = 1;
+
+for(var i = 1; i < row; i++)
+{
+    numbers[i] = (numbers[i-1]) * ((n+1-k)/k);
+    k++;
+}
+numbers[numbers.length] = 1;
+//console.log(numbers);
+return [numbers];
+}
+
+
+// Generate a certain level of Pascal's pyramid.
+
+function generatePascalPyramidLevel(level)
+{
+
+    // First we need the row of pascal's triangle at this level.
+    var trianglerow = generatePascalTriangleRow(level);
+    console.log(trianglerow);
+
+    var pyramidlevel = [];
+    for(var i = 0; i <= level; i++) // i is a row in this case.
+    {
+        var levelrow = [];
+        var multiplicationRow = generatePascalTriangleRow(i);
+        if(i == 0)
+        {
+            // Yes this has the first "row" of our triangle as a base...
+            // the first one will always be zero.. so
+            levelrow = [1];
+        }else
+            {
+        /*    console.log("multiplication row");
+            console.log(multiplicationRow[0]);
+            console.log(multiplicationRow[0].length);
+          */  for(var k = 0; k < multiplicationRow[0].length; k++)
+            {
+       //         console.log("calculating " + multiplicationRow[0][k] + " mutliplied with " + trianglerow[0][i]);
+                levelrow[k] = multiplicationRow[0][k] * trianglerow[0][i];
+            }
+        }
+        pyramidlevel[i] = levelrow;
+    }
+    /*console.log(levelrow);
+    console.log("Pyramid level: ");
+    console.log(pyramidlevel);
+*/
+    // Now for every row of pascal triangle, up to the level, we multiply each number by the trianglerow which gives us the pyramid. In theory.
+    return pyramidlevel;
+
 }
--- js/world.js
+++ js/world.js
@@ -2,7 +2,7 @@
  * Created by Dylan on 27/08/2015.
  */
 
-
+var exists = false;
 var pascalnumbers = [];
 function getPascalTriangle(rows) {
     for (var row = 1; row < rows; row++) {
@@ -10,30 +10,41 @@ function getPascalTriangle(rows) {
     }
 }
 
+// Billboard & 2-sided textures.
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 function displayTriangle() {
-    getPascalTriangle(10);
-    console.log(THREE.FontUtils.faces);
+    // fetch the amount of rows from the page.
+    // Each time it is pressed we need to refresh the page
+
+    // We should choose an initial spacing between numbers based on the input amount of numbers (they get larger near the bottom)
+    var rowinput = 1;
+    rowinput = document.getElementById("rows").value;
+    getPascalTriangle(rowinput);
+
     var scene = new THREE.Scene();
-
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    renderer.setSize(window.innerWidth, window.innerHeight-125);
+    if(!exists){
+        document.getElementById("renderframe").appendChild(renderer.domElement);
+        exists = true;
+    }else{ // we empty the scene
 
+        while(document.getElementById("renderframe").hasChildNodes()){
+            document.getElementById("renderframe").removeChild(document.getElementById("renderframe").lastChild);
+        }
+        scene = new THREE.Scene();
+        document.getElementById("renderframe").appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    var cube = new THREE.Mesh(geometry, material);
-    // scene.add( cube );
+    }
 
     camera.position.z = 5;
 
     // generate all the shapes
-    console.log(pascalnumbers);
+    //console.log(pascalnumbers);
 
 
-    console.log(currentNumber);
+    //console.log(currentNumber);
     var shapes, geom, mat, mesh;
 
     // hardcode the start for a bit
@@ -46,32 +57,41 @@ function displayTriangle() {
 
     geom = new THREE.ShapeGeometry(shapes);
     mat = new THREE.MeshBasicMaterial();
+    mat.side = THREE.DoubleSide;
     mesh = new THREE.Mesh(geom, mat);
     mesh.position.set(1.5, +2.5, 0);
+
     scene.add(mesh);
+
 
     // end hardcode
     var previousNumber = 1;
     var currentNumber; // We need the # numbers of this to adjust the offset correctly!
     for (var row = 0; row < pascalnumbers.length; row++) {
-        console.log("new row # .. " + row);
+        //console.log("new row # .. " + row);
         var xoffset = -row/2; // start of x-offset.
         for (var col = 0; col <= row + 1; col++) {
             currentNumber = pascalnumbers[row][0][col];
             console.log(currentNumber);
-            shapes = THREE.FontUtils.generateShapes(pascalnumbers[row][0][col], {
+            shapes = THREE.FontUtils.generateShapes(Math.round(currentNumber), {
                 font: "helvetiker",
                 weight: "normal",
                 size: 0.25
             });
             // the x Offset depends on the "length" of the previous number & the position.
             var numberCorrection = previousNumber.toString().length /10 ;
-            console.log(numberCorrection);
-            xoffset += 1
+        //    console.log(numberCorrection);
+
+            // The space between the numbers is determined by the amount of rows we generated.
+
+
+
+            xoffset += 1; // We need to fix this for really large numbers!
             var yoffset = 2 - row / 2;
             var zoffset = 0;
             geom = new THREE.ShapeGeometry(shapes);
             mat = new THREE.MeshBasicMaterial();
+            mat.side = THREE.DoubleSide;
             mesh = new THREE.Mesh(geom, mat);
             mesh.position.set(xoffset, yoffset, zoffset);
             scene.add(mesh);
@@ -79,7 +99,9 @@ function displayTriangle() {
             previousNumber = currentNumber;
         }
     }
+
     function render() {
+
         requestAnimationFrame(render);
         renderer.render(scene, camera);
     }
@@ -88,15 +110,156 @@ function displayTriangle() {
 
 
 
+//***************** END PASCAL TRIANGLE *****************************\\
 
 
 
 
+//*************** BEGIN PASCAL PYRAMID ******************************\\
+
+
+var pascalpyramid = [1];
+function getPascalPyramid(levels) // levels indicate 'floors' on the y-axis.
+{
+    for(var row = 1; row <= levels; row++)
+    {
+        console.log("generating level: " + row);
+        pascalpyramid[row-1] = generatePascalPyramidLevel(row);
+    }
+}
+
+
+function displayPyramid()
+{
+
+
+
+    // set up renderer and camera
+    var scene = new THREE.Scene();
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight-125);
+    document.getElementById("renderframe").appendChild(renderer.domElement);
+    camera.position.z = 5;
+    // end setup
+
+    var input = document.getElementById("rows").value;
+    // hardcoded the amount of rows for now.
+    console.log(input);
+    getPascalPyramid(input);
+
+
+    // just print what I have now
+    console.log("pascal's pyramid");
+    console.log(pascalpyramid);
+
+    console.log("in-loop");
+    var shapes, geom, mat, mesh;
+
+
+
+    // hardcode the number 1 for a sec
+
+
+    shapes = THREE.FontUtils.generateShapes("1", {
+        font: "helvetiker",
+        weight: "normal",
+        size: 0.25
+    });
+
+    xoffset += 1; // We need to fix this for really large numbers!
+    geom = new THREE.ShapeGeometry(shapes);
+    mat = new THREE.MeshBasicMaterial({color:colour});
+    mat.side = THREE.DoubleSide;
+    mesh = new THREE.Mesh(geom, mat);
+    mesh.position.set(1, 2.50, 0);
+    scene.add(mesh);
+
+
+
+    // end hardcoded 1
+
+
+    var xoffset = 0;
+    var yoffset = 2;
+    var zoffset = 0;
+    var colour = 0xffff0000;
+    for(var level = 0; level < pascalpyramid.length; level++)
+    {
+        zoffset = 0;
+        // console.log(pascalpyramid[level]);
+        for(var row = 0; row < pascalpyramid[level].length;row++)
+        {
+            xoffset = 0;
+            //console.log(pascalpyramid[level][row]);
+            for(var col = 0; col < pascalpyramid[level][row].length; col++)
+            {
+            //      console.log(pascalpyramid[level][row][col]);
+
+
+                var currentNumber = pascalpyramid[level][row][col];
+
+
+                shapes = THREE.FontUtils.generateShapes(Math.round(currentNumber), {
+                    font: "helvetiker",
+                    weight: "normal",
+                    size: 0.25
+                });
+
+                xoffset += 1; // We need to fix this for really large numbers!
+                geom = new THREE.ShapeGeometry(shapes);
+                mat = new THREE.MeshBasicMaterial({color:colour});
+                mat.side = THREE.DoubleSide;
+                mesh = new THREE.Mesh(geom, mat);
+                mesh.position.set(xoffset, yoffset, zoffset);
+                scene.add(mesh);
+
+            }
+            // Adjust the Z-axis for every column (so we display columns along Z
+            zoffset -= 0.5;
+        }
+        // Adjust Y-axis for every level.
+        // change the colour for the level.
+        if(colour === 0xffff0000){
+            colour = 0xff0000ff;
+        }
+        else
+        {
+            colour = 0xffff0000;
+        }
+        yoffset -= 1;
+    }
+
+
+
+    function render() {
+        console.log("rendering");
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+
+        // bill board effect goes here.
+        scene.traverse(function (node)
+        {
+            if(node instanceof THREE.Mesh)
+            {
+                node.quaternion.copy(camera.quaternion);
+            }
+        });
+    }
+    render();
+}
+
+
+
+
+//*************** END PASCAL PYRAMID ******************************\\
+
+
+//*************** BEGIN CAMERA MANIPULATION ******************************\\
 
 // testing camera movement
 
 THREE.PerspectiveCamera.prototype.setRotateX = function( deg ){
-    if ( typeof( deg ) == 'number' && parseInt( deg ) == deg ){
+    if ( typeof( deg ) ==  'number' &&parseInt( deg ) == deg ){
         this.rotation.x = deg * ( Math.PI / 180 );
     }
 };
@@ -138,7 +301,7 @@ const Q = 81;
 const distance = 0.25; // how much does the camera move during forward/backward/left/right
 document.addEventListener('keydown', function(e) {
     var key = e.keyCode;
-    console.log(key);
+    //console.log(key);
 
     switch (key) {
 
